4d6541f58970aae665b0eb92ecf4fa38437ceb27
Andreas Burger <se15m020@technikum-wien.at>
2016-05-31 17:50:58 +0200

    enhance TimeLogger

--- src/main/java/at/fhtw/hpc/util/TimeLogger.java
+++ src/main/java/at/fhtw/hpc/util/TimeLogger.java
@@ -1,20 +1,22 @@
 package at.fhtw.hpc.util;
 
+
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+
 public class TimeLogger {
 
 	private long start;
 	
 	public void start()	{
-		start = System.currentTimeMillis();
+		start = System.nanoTime();
 	}
 
 	public void end(String step) {
-		long diff = System.currentTimeMillis() - start;
-		
-		int minutes = (int) (diff / 60000.0);
-		int seconds = (int) (diff / 1000.0) - (minutes * 60);
-		int millisecs = (int) (((diff / 1000.0) - (seconds + minutes * 60)) * 1000);
-
-		System.out.printf("%-20s %1d:%02d.%03d\n", step, minutes, seconds, millisecs);
+		long diff = System.nanoTime() - start;
+		long seconds = NANOSECONDS.toSeconds(diff);
+		long millis = NANOSECONDS.toMillis(diff) % 1000;
+		long micros = NANOSECONDS.toMicros(diff) % 1000;
+		long nanos = NANOSECONDS.toNanos(diff) % 1000;
+		System.out.printf("%-20s %2d s, %3d ms, %3d µs, %3d ns\n", step, seconds, millis, micros, nanos);
 	}
 }
