a00c9e150238046ffe017ce1b6c8944df1ae9224
Johanna Strutzenberger <johanna.strutzenberger@bildkopie.com>
2016-06-06 22:22:28 +0200

    Vergleich von Seriell und Parallel

---
+++ src/main/java/at/fhtw/hpc/exercise2/ScanComparer.java
@@ -0,0 +1,41 @@
+package at.fhtw.hpc.exercise2;
+
+import at.fhtw.hpc.util.ExecutionStatisticHelper2;
+
+import java.util.Random;
+
+/**
+ * Created by JS on 06/06/2016.
+ */
+public class ScanComparer {
+
+
+	public static ExecutionStatisticHelper2 ExecutionStatisticHelper = new ExecutionStatisticHelper2();
+
+	public static void main(String args[]) {
+		// fill array with random numbers
+		Random random = new Random();
+
+		int size = (int) Math.pow(2, 8);
+		System.out.println("Perform scan with array size: " + size);
+
+		float[] input = new float[size];
+		for (int i = 0; i < input.length; i++) {
+			input[i] = (float) random.nextInt(9999);
+		}
+
+		float serialOutput[] = Scan.performScan(input);
+
+		ScanComparer.ExecutionStatisticHelper.clear();
+		float parallelOutput[] = WorkEfficientParallelScan.performScan(input);
+		ScanComparer.ExecutionStatisticHelper.printSummary();
+
+		// compare results
+		for (int i = 0; i < serialOutput.length; i++) {
+			if (parallelOutput[i] != serialOutput[i]) {
+				System.out.println("Scan was wrong at index: " + i + ". (" + serialOutput[i] + " vs. " + parallelOutput[i] + ")");
+				return;
+			}
+		}
+	}
+}
